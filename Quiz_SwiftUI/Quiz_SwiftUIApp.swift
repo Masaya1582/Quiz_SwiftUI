3d7863f71b2771c56170df6eb12484065a35478f
Masaya1582 <masa8y88@gmail.com>
2023-06-25 13:27:11 +0900

    Initial Commit

---
+++ Quiz_SwiftUI/Quiz_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_SwiftUIApp.swift
+//  Quiz_SwiftUI
+//
+//  Created by MasayaNakakuki on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
